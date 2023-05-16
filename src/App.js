e2e64cdab52fdb528b2b7a9e1e9618db3dadb1ef
Pujangi Darekar <pujangi.d@prometteur.in>
2023-05-16 16:00:09 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>learn react</p>
         <h1>This is demo of CI/CD Pipeline</h1>
         Hello world
       </header>
